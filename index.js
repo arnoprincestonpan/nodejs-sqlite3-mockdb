8e6acfb5d5a233b1cb78c24e87d0a43b1c377c5f
panarno <78936893+panarno@users.noreply.github.com>
2023-02-16 03:56:43 -0800

    added random user api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const sqlite3 = require("sqlite3").verbose()
+const axios = require("axios")
 
 const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, (err) => {
     if(err) return console.error(err.message)
@@ -6,14 +7,33 @@ const db = new sqlite3.Database('./mock.db', sqlite3.OPEN_READWRITE, (err) => {
     console.log("connection successful")
 })
 
-db.run("CREATE TABLE IF NOT EXISTS users(first_name, last_name, user_name, password, email, id)")
+axios
+    .get("https://random-data-api.com/api/users/random_user")
+    .then((response) => {
+        // destructuring data
+        const {data} = response;
+        const {first_name} = data;
+        const {last_name} = data;
+        const {username} = data;
+        const {email} =data;
+        const {id} = data;
+        const {password} = data;
 
-const sql = "INSERT INTO users(first_name, last_name, user_name, password, email, id) VALUES(?,?,?,?,?,?)"
+        db.run("CREATE TABLE IF NOT EXISTS users(first_name, last_name, username, password, email, id)")
 
-db.run(sql, ["mike", 'codes', 'mike_codes', '123', 'mikec@mail.com', 1], (err) => {
-    if(err) return console.error(err.message)
-    console.log("a new user has been inserted.")
-})
+        const sql = "INSERT INTO users(first_name, last_name, username, password, email, id) VALUES(?,?,?,?,?,?)"
+        
+        db.run(sql, [first_name, last_name, username, password, email, id], (err) => {
+            if(err) return console.error(err.message)
+            console.log("a new user has been inserted.")
+        })
+
+        db.close((err) =>{
+            if(err) return console.error(err.message)
+        })
+    }).catch((error) => {
+        if (error) return console.error(error)
+    })
 
 const users = "SELECT * FROM users"
 
@@ -24,8 +44,4 @@ db.all(users, [], (err, rows) => {
     rows.forEach(row => {
         console.log(row)
     })
-})
-
-db.close((err) =>{
-    if(err) return console.error(err.message)
 })
